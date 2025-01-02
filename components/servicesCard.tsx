b3016409f12e8b9440c3668421049d8ee9c0c5ac
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-01-02 21:15:22 +0200

    solving bugs of deploying

--- components/servicesCard.tsx
+++ components/servicesCard.tsx
@@ -12,7 +12,7 @@ const ServicesCard: React.FC<ServicesCardProps> = ({
   image,
 }) => {
   return (
-    <div className="flex  flex-col md:flex-row items-center bg-gradient-to-b to-gray-500 from-black m-auto text-white px-4 rounded-lg shadow-lg h-auto w-auto mx-auto">
+    <div className="flex flex-col md:flex-row items-center bg-gradient-to-b to-gray-500 from-black m-auto text-white px-4 rounded-lg shadow-lg h-auto w-auto mx-auto">
       <div className="flex-1">
         <h1 className="text-2xl font-bold text-[#928c6b] mb-2">{heading}</h1>
         <p className="text-lg leading-relaxed mb-4">{description}</p>
