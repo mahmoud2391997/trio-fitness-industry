c758ab5b11e790fa8e99fb510d5456bc8fc4f118
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-01-20 15:57:27 +0200

    modifying offers

--- components/limitedOffersSection.jsx
+++ components/limitedOffersSection.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import PackageCard from './packageCard'
 import PackageComponent from './packageComponent'
+import { title } from 'process';
 
 function LimitedOffersSection() {
   const packages = [
     {
         id: 1,
-        title: "3 MONTH + 1 MONTH FREE",
-        description: "Subscribe for 3 months and get an additional month free. A great way to enjoy our services with extra value."
-    },
-    {
+        // title: "3 MONTH + 1 MONTH FREE",
+        title:"Personalized Nutrition Plan Offers",
+        description: ["Subscribe for 3 months and get an additional month free, With 33% DISCOUNT.","Subscribe for 6 months, With 33% DISCOUNT. A great way to enjoy our services with extra value."]
+      },
+      {
         id: 2,
-        title: "6 MONTH + 2 MONTH FREE",
-        description: "Subscribe for 6 months and receive 2 extra months for free. Enjoy extended benefits and savings."
+        // title: "6 MONTH + 2 MONTH FREE",
+        title:"Customized Training & Nutrition Plan Offers",
+        description: ["Subscribe for 3 months and get an additional month free, With 43% DISCOUNT.","Subscribe for 6 months, With 38% DISCOUNT. A great way to enjoy our services with extra value."]
     },
 ];
 
@@ -20,7 +23,7 @@ function LimitedOffersSection() {
     <div className='w-full bg-black text-white p-7'  style={{background:"url(/aboutbg.jpeg)", backgroundSize:"cover"}}>
             <h2 className="xl:text-5xl w-[70%] lg:w-[50%] m-auto lg:text-5xl md:text-4xl sm:text-3xl text-2xl  text-center font-bold mb-6 text-[#928c6b]">DON'T MISS OUT OUR LIMITED-TIME OFFERS!</h2>
 
-      <div className="packages w-[80%] grid grid-cols-1 md:grid-cols-2  h-auto gap-5 m-auto">
+      <div className="packages w-[70%] grid grid-cols-1 md:grid-cols-2  h-auto gap-4 m-auto">
         {packages.map((pkg) => (
           <PackageComponent
             key={pkg.id}
--- components/packageComponent.tsx
+++ components/packageComponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface PackageComponentProps {
   packageName: string;
-  packageDescription: string;
+  packageDescription: string[];
 }
 
 const PackageComponent: React.FC<PackageComponentProps> = ({
@@ -10,17 +10,18 @@ const PackageComponent: React.FC<PackageComponentProps> = ({
   packageDescription,
 }) => {
   return (
-    <div className="relative min-h-[20vh] p-2 pb-16 rounded-xl  border border-white shadow-md">
+    <div className="relative  h-auto p-2 pb-16 rounded-xl  border border-white shadow-md">
       
-        <div className="h-[35%] rounded-lg bg-white">
+        <div className="h-[35%] rounded-b-full bg-white ">
           <h2 className="text-2xl sm:text-4xl text-center text-[#928c6b] font-bold mb-2">
             {packageName}
           </h2>
         </div>
-        <div className="h-[50%] rounded-lg my-3">
-          <h3 className="text-lg sm:text-2xl text-center text-white font-semibold mb-2">
-            {packageDescription}
-          </h3>
+        <div className="h-[70%] rounded-lg my-3">
+          {packageDescription.map((packagee)=>{return <h3 className="text-lg sm:text-2xl text-center text-white font-semibold mb-2">
+            {packagee}
+          </h3>})}
+          
         </div>
        
       <button
