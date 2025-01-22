56d9d2487f10dc12da138dfb0a8a90148eaba911
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-01-22 20:30:01 +0200

    deploy fixing

--- components/limitedOffersSection.tsx
+++ components/limitedOffersSection.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import PackageCard from './packageCard'
 import PackageComponent from './packageComponent'
-import { title } from 'process';
 
 function LimitedOffersSection() {
   const packages = [
