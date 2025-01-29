026e3141036caf15b7ab55108b1f33d47bfe126e
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-01-29 23:54:13 +0200

    trying to fix the bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,30 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import Footer from "@/components/footer";
-import LanguageProvider from "../context/LanguageContext";
+import LanguageProvider, { useLanguage } from "../context/LanguageContext";
+import { Cairo } from "next/font/google"; // Import Cairo font for Arabic
+import { Geist, Geist_Mono } from "next/font/google";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+const cairoFont = Cairo({
+  variable: "--font-cairo",
+  subsets: ["arabic"],
+});
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }: AppProps) {
+  const {language} = useLanguage();
   return (
     <Provider store={store}>
       <LanguageProvider>
-        <div className="relative">
+        <div  className={`${
+        language === "arabic" ? cairoFont.variable : geistSans.variable
+      } ${geistMono.variable}relative`}>
           <SocialMedia />
           <NavBar navBg={"bg-black"} />
           <Component {...pageProps} />
